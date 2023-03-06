83ce9776dadecf4e7c6c400479e4f9a847b1c9d9
joemk2 <36236535+joemk2@users.noreply.github.com>
2023-03-06 17:20:07 +0900

    Add files via upload

---
+++ Azure Synapse/LAB File/SQL Script/02_Create_Credential.sql
@@ -0,0 +1,11 @@
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Demo@pass123';
+CREATE DATABASE SCOPED CREDENTIAL MigrationCredential
+WITH IDENTITY = '<ADLS Gen2 Account Name>' , SECRET = '<ADLS Gen2 Key>'
+
+CREATE EXTERNAL DATA SOURCE MigrationStor WITH (TYPE = HADOOP,
+LOCATION=
+'wasbs://exportfile@<ADLS Gen2 Account Name>.blob.core.windows.net',
+CREDENTIAL = MigrationCredential);
+
+CREATE EXTERNAL FILE FORMAT MigrationFiles WITH(FORMAT_TYPE = DelimitedText,
+FORMAT_OPTIONS (FIELD_TERMINATOR = '|'));
